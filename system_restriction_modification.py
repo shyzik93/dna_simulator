60b2eaf21a3f1cf66445379545fd7935af64b8fd
shyzik93 <shyzik93@mail.ru>
2020-10-18 08:46:32 +0300

    Добавлена система рестрикции-модификации.

--- system_restriction_modification.py
+++ system_restriction_modification.py
@@ -1,6 +1,8 @@
 class SystemRestrictionModification:
-    def __init__(self, enzymes):
+    def __init__(self, enzymes, restriction_sites):
         """
         Объект системы Рестрикции-Модификации
         :param enzymes: список ферментов рестриктазы и/или метилтрансферазы
         """
+        self.enzymes = enzymes
+        self.restriction_sites = restriction_sites
