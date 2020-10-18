f438b8c9dcee6d2efe60b1f637228746f05d817b
shyzik93 <shyzik93@mail.ru>
2020-10-18 05:55:28 +0300

    Обьект Restrictase добавлен

---
+++ restrictase.py
@@ -0,0 +1,13 @@
+class Restrictase:
+    """
+    Объект фермента Рестриктаз (эндонуклеаза рестрикции)
+    """
+    def __init__(self):
+        pass
+
+    def restriction(self):
+        """
+        Рестрикция - разрезание ДНК на ферменты
+        :return:
+        """
+        return None
