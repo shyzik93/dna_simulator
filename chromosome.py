a28fedda36f42c8e0437dc13cba74de3f3b2ae44
shyzik93 <shyzik93@mail.ru>
2020-10-18 05:33:31 +0300

    Новый раздел добавлен. Добавлена пустая функция рестрикции

--- chromosome.py
+++ chromosome.py
@@ -4,21 +4,29 @@ class Chromosome:
 
     def replicate(self):
         """
-        Репликация: синтез ДНК по матрице ДНК
+        Репликация - синтез ДНК по матрице ДНК
         :return: копия ДНК
         """
         return None
 
     def transcription(self):
         """
-        Транскрипция: синтез РНК по матрице ДНК
+        Транскрипция - синтез РНК по матрице ДНК
         :return: РНК
         """
         return None
 
     def translation(self):
         """
-        Ттрансляция: синтез белка по матрице РНК
+        Трансляция - синтез белка по матрице РНК
         :return: белок
+        """
+        return None
+
+    def restriction(self, *restriction_sites):
+        """
+        Рестрикция - разрезание ДНК на фрагменты
+        :param restriction_sites: сайты рестрикции - участки ДНК, по которым осуществляется разрезание
+        :return:
         """
         return None
